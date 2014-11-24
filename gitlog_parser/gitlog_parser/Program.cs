abf2476b86cc5567cb86334c74684b9005c4b03c
Sanghee4346 <sanghee4346@naver.com>
2014-11-25 02:03:31 +0900

    only delection

--- gitlog_parser/gitlog_parser/Program.cs
+++ gitlog_parser/gitlog_parser/Program.cs
@@ -12,8 +12,9 @@ namespace gitlog_parser
         {
             Parsing_class a = new Parsing_class();
             ArrayList logData = new ArrayList();
+            ArrayList parsing_logData = new ArrayList();
             logData = a.fileRead();
-            a.commitParsing(logData);
+            parsing_logData = a.commitParsing(logData);
         }
     }
     class Parsing_class
@@ -24,14 +25,14 @@ namespace gitlog_parser
              * 2014-11-19 권상희
              * 로그 파일이 시간 역순으로 배치되어 있으므로, 개발초기부터 로그를 배치하여 읽도록 2개의 Arraylist를 이용하였습니다.
              * string line : file을 line당 읽어서 이용하기 위한 String Type 변수
-            */          
+            */
             ArrayList temp_logTotalData = new ArrayList();
             ArrayList logTotalData = new ArrayList();
             int counter = 0;
             string line;
             // Read the file and display it line by line.(파일 포인터)
             System.IO.StreamReader file =
-            new System.IO.StreamReader(@"C:\Users\내문서\Source\Repos\Log_Analizer\gitlog_parser\gitlog_parser\log_stat_WE.txt");
+            new System.IO.StreamReader(@"C:\Users\내문서\Source\Repos\Log_Analizer\gitlog_parser\gitlog_parser\log_stat.txt");
             // Text file로 부터 읽어서 프로그램 내장화
             while ((line = file.ReadLine()) != null)
             {
@@ -54,8 +55,8 @@ namespace gitlog_parser
             //System.Console.ReadLine();
             return logTotalData;
         }
-        public void commitParsing(ArrayList logData)
-        {   
+        public ArrayList commitParsing(ArrayList logData)
+        {
             /*
              * 2014-11-24 권상희
              * 0번 index : insertion,  1번 index : deletion, 3번 index : date 4번 index : e-mail 5번 index : author
@@ -63,67 +64,94 @@ namespace gitlog_parser
             */
             // totalCommitValue : Log의 최종 commit수 변수
             int totalCommitValue = 0;
+            ArrayList tem_Parsing_Data = new ArrayList();
             ArrayList Parsing_Data = new ArrayList();
             string data = null;
-            
             for (int i = 0; i < logData.Count; i++)
             {
-                if (logData[i].ToString().Contains("changed") == true)
+                data = data + '%' + logData[i].ToString();
+                if (logData[i].ToString().Contains("Author") == true)
                 {
-                    if(logData[i].ToString().Contains("insertion")==true)
+                    //System.Console.WriteLine("data 값은\n"+data);
+                    tem_Parsing_Data.Add(data);
+                    data = null;
+                    i++;
+                }
+            }
+            System.Console.WriteLine("count : " + tem_Parsing_Data.Count + "\n");
+            totalCommitValue = tem_Parsing_Data.Count;
+            // 검증
+            /*for (int j = 0; j < tem_Parsing_Data.Count; j++)
+            {
+                System.Console.WriteLine(tem_Parsing_Data[j].ToString());
+            }*/
+            for(int i = 0; i <tem_Parsing_Data.Count;i++)
+            {
+                data = tem_Parsing_Data[i].ToString();
+                if(data.Contains("changed") == true)
+                {
+                    if (data.Contains("insertion") == true)
                     {
-                        data = logData[i].ToString();
-                        string[] cut_str = data.Split(new char[]{',',' '});
-                        for(int j=0 ; j<cut_str.Length;j++)
+                        string[] cut_str = data.Split(new string[]{",","insert","dele"},StringSplitOptions.RemoveEmptyEntries);
+                        cut_str[1] = cut_str[1].Trim();
+                        Parsing_Data.Add(cut_str[1]);
+                        if(cut_str.Length > 4)
                         {
-                            System.Console.WriteLine((j+1)+"번째"+cut_str[j]);
+                            if (cut_str[4].Contains("(-)") == true)
+                            {
+                                cut_str[3] = cut_str[3].Trim();
+                                Parsing_Data.Add(cut_str[3]);
+                            }
                         }
-                        data = cut_str[5].Trim();
-                        System.Console.WriteLine("insertion 집어넣을 데이터" + data);
-                        Parsing_Data.Add(data);
-                        if (cut_str.Length > 7)
+                        else
                         {
-                            data = cut_str[8].Trim();
-                            System.Console.WriteLine("delection 집어넣을 데이터" + data);
-                            Parsing_Data.Add(data);
-                        }
-                    }
-                    else 
-                    {
-                        data = null;
-                        Parsing_Data.Add(data);
-                    }
-                    if(logData[i].ToString().Contains("deletion")==true)
-                    {
-                        data = logData[i].ToString();
-                        string[] cut_str = data.Split(new char[] { ',', ' ' });
-                        if(cut_str[6].Contains("delection")==true)
-                        {
-                            data = cut_str[5].Trim();
-                            System.Console.WriteLine("delection 집어넣을 데이터" + data);
-                            Parsing_Data.Add(data);
+                            Parsing_Data.Add("0");
                         }
                     }
                     else
                     {
-                        data = null;
-                        Parsing_Data.Add(data);
-                    }               
+                        Parsing_Data.Add("0");
+                        if (data.Contains("deletion") == true)
+                        {
+                            string[] cut_str = data.Split(new string[] { ",", "deletion" }, StringSplitOptions.RemoveEmptyEntries);
+                            cut_str[1] = cut_str[1].Trim();
+                            Parsing_Data.Add(cut_str[1]);
+                        }
+                        else 
+                        {
+                            Parsing_Data.Add("0");
+                        }
+                    }
                 }
-                else 
+                else
                 {
-                    data = null;
-                    Parsing_Data.Add(data);
-                    Parsing_Data.Add(data);
+                    Parsing_Data.Add("0");
+                    Parsing_Data.Add("0");
+                }
+                if (data.Contains("Date:") == true)
+                {
+                    string[] cut_str = data.Split(new string[] { "Date:", "Author:"}, StringSplitOptions.RemoveEmptyEntries);
+                    for (int j = 0; j < cut_str.Length; j++)
+                    {
+                        cut_str[j] = cut_str[j].Trim();
+                        //System.Console.WriteLine(j+"번째" + cut_str[j]);
+                    }
+                    Parsing_Data.Add(cut_str[1]);
+                    string[] name_data = cut_str[2].Split(new char[] { '<', '>' }, StringSplitOptions.RemoveEmptyEntries);
+                    for (int j = 0; j < name_data.Length; j++)
+                    {
+                        name_data[j] = name_data[j].Trim();
+                    }
+                    Parsing_Data.Add(name_data[0]);
+                    Parsing_Data.Add(name_data[1]);
                 }
             }
-            for (int i = logData.Count - 1; i <= 0; i--)
+            // total test
+            for(int j = 0;j < Parsing_Data.Count;j++)
             {
-                if (logData[i].ToString().Contains("Author") == true)
-                {
-                    totalCommitValue = totalCommitValue + 1;
-                }
+                System.Console.WriteLine(Parsing_Data[j].ToString());
             }
+            return Parsing_Data;
         }
     }
 }
